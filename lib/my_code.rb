2b304fb7a364a71c126a57eb464e1f8ef4d4c968
Zach Brown <zachmb22@gmail.com>
2020-01-25 21:41:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -67,8 +67,8 @@ def reduce_to_any_true(source_array)
   while i < source_array.length
     if source_array[i] == true
       return true
-      i += 1
     end
+    i += 1
   end
       return false
 
