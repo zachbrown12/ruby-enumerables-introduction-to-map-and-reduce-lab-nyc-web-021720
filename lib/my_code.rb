fe420d692fe79b971b694d9c21c6a787ba115c3d
Zach Brown <zachmb22@gmail.com>
2020-01-25 21:42:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length
-    if source_array[i] == "false"
+    if source_array[i] == false
       return false
     end
     i += 1
